e377d884e2ca31547c94b0ef41ec942871bc4ee6
Jay Winebrenner <jaywinebrenner@gmail.com>
2020-04-08 10:00:45 -0700

    tweak support screen

---
+++ screens/client/SupportScreen.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+import { AuthContext } from "../../context";
+import ClientHeader from '../../components/client/ClientHeader'
+
+const SupportScreen = ({ navigation }) => {
+  
+
+  return (
+    <View style={styles.container}>
+      <ClientHeader />
+      <Text>Support Screen</Text>
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  button: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    marginVertical: 10,
+    borderRadius: 5
+  }
+});
+
+export default SupportScreen

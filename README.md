80c129b55e80cff31aebd9c9a2c3ca6224fed90e
Lee Pedersen <leepedersenr@gmail.com>
2020-04-27 16:02:00 -0600

    try and make github happy with my changes

--- README.md
+++ README.md
@@ -8,6 +8,18 @@
 * scrollview needs bottom margin w/ keyboard
 * on android, the back button is a blank white square
 
-## Other bugs
+
+Embedded image is SLOW to load on Client Support Screen / Client About Page
+
+
+* Warnings on app
+"Virtualized lists should never be nested inside plain scrollviews with the same orientation" error on PilotProfileSetupPageOneScreen
+"possilbe unhandled promise rejection id:0" on PilotProfileSetUpPageOneScreen
+
+* Dropdown clean up on PilotProfileSignupForms
+
+* Deep look at condictional rendering between ios / android
+
+## Other
 
 * on Lee's phone, toggling between tabs sometimes doesn't work
---
+++ components/auth/HomeImage.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { View, TouchableOpacity, StyleSheet, Text, ImageBackground } from "react-native";
+import landingPageImage from "../../assets/landingPageImage.png";
+
+function HomeImage({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <ImageBackground source={landingPageImage} style={styles.backgroundImage}>
+        <Text style={styles.imageText}>PROFESSIONAL DRONE SERVICES</Text>
+
+        <Text style={styles.imageTextTwo}>
+          THE FASTEST WAY TO GET AERIAL IMAGES AND DATA
+        </Text>
+      </ImageBackground>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height: "100%",
+    width: "110%",
+    zIndex: 0
+  },
+  backgroundImage: {
+    // marginTop:
+    flex: .7,
+    // zIndex: 0
+    // height: "100%",
+    // width: "100%",
+    // position: "absolute",
+  },
+  imageText: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "white",
+    textAlign: "left",
+    marginTop: "40%",
+    marginLeft: 10,
+  },
+  imageTextTwo: {
+    fontSize: 13,
+    fontWeight: "bold",
+    color: "#3E90D0",
+    textAlign: "left",
+    marginTop: 10,
+    marginBottom: 100,
+    marginLeft: 10,
+  },
+});
+
+export default HomeImage;

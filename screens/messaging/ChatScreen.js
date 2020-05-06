a2b57eb95366ea65f57bec24b71d1d34687ae3fa
George Acosta <georgeacostar@gmail.com>
2020-05-06 10:42:28 -0700

    add keyboard avoidance to messeges and add styles

--- screens/messaging/ChatScreen.js
+++ screens/messaging/ChatScreen.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { TouchableOpacity,
+import {
+  TouchableOpacity,
   View,
   Text,
   StyleSheet,
@@ -8,22 +9,27 @@ import { TouchableOpacity,
   TextInput,
   FlatList,
   TouchableHighlight,
-  Alert
-  } from "react-native";
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Platform,
+  Container,
+} from "react-native";
 import {
   Ionicons,
   FontAwesome5,
-  MaterialCommunityIcons
+  MaterialCommunityIcons,
+  Alert,
+  AntDesign,
 } from "@expo/vector-icons";
 import { connect } from "react-redux";
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation } from "@react-navigation/native";
 import { getMessages, postMessages } from "../../actions/index";
-import * as firebase from 'firebase';
+import * as firebase from "firebase";
 import _ from "lodash";
-import { AntDesign } from "@expo/vector-icons";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 function ChatScreen(props, { getMessages, postMessages }) {
-
   const navigation = useNavigation();
 
   const [body, setBody] = useState("");
@@ -45,11 +51,15 @@ function ChatScreen(props, { getMessages, postMessages }) {
   let conversation = [];
   if (sender && recipient) {
     props.listOfMessages.forEach((message) => {
-      if ((message.userOneID === sender.uid && message.userTwoID === recipient.userID) || (message.userOneID === recipient.userID && message.userTwoID === sender.uid)) {
+      if (
+        (message.userOneID === sender.uid &&
+          message.userTwoID === recipient.userID) ||
+        (message.userOneID === recipient.userID &&
+          message.userTwoID === sender.uid)
+      ) {
         conversation.push(message);
       }
     });
-    // change message to read
   }
 
   const send = (e) => {
@@ -63,135 +73,122 @@ function ChatScreen(props, { getMessages, postMessages }) {
     let timestamp = new Date();
     props.postMessages(sender.uid, recipient.userID, timestamp, body);
     setBody("");
-  }
-
+  };
 
   return (
-    <View>
-      <View style={styles.inputBackButtoncontainer}>
-        <View style={styles.inputWrapper}>
-          <TextInput
-          multiline={true}
-            autoCorrect={true}
-            placeholder="Send message..."
-            placeholderTextColor="grey"
-            value={body}
-            onChangeText={setBody}
-            style={styles.input}
-          />
-          <AntDesign 
-            style={styles.plusIcon} 
-            name="plus" 
-            size={25}
-            onPress={send} 
-          />
-        </View>
+    <KeyboardAwareScrollView
+      style={{
+        flex: 1,
+        height: "100%",
+        backgroundColor: "lightgray",
+      }}
+    >
+      <FlatList
+        style={{ width: "100%", marginTop: 20 }}
+        data={conversation}
+        keyExtractor={(item) => item.key}
+        renderItem={({ item }) => {
+          return (
+            <View
+              style={{
+                elevation: 8,
+                borderRadius: 15,
+                backgroundColor: "#3E90D0",
+                marginBottom: 15,
+                padding: 20,
+              }}
+            >
+              <View>
+                <Text>Message sent at: {item.timestamp}</Text>
+                <Text>Message body: {item.body} </Text>
+              </View>
+            </View>
+          );
+        }}
+      />
+
+      <View
+        style={{
+          backgroundColor: "lightgray",
+          marginTop: 10,
+          marginBottom: 40,
+          alignItems: "center",
+        }}
+      >
+        <TextInput
+          placeholder=" Send message..."
+          placeholderTextColor="grey"
+          value={body}
+          onChangeText={setBody}
+          style={{
+            width: "80%",
+            borderWidth: 4,
+            backgroundColor: "white",
+            borderRadius: 20,
+            height: 45,
+            marginBottom: 10,
+            marginTop: 20,
+          }}
+        />
+        <AntDesign
+          style={{
+            position: "absolute",
+            top: "18%",
+            right: "15%",
+            color: "#092455",
+          }}
+          name="plus"
+          size={25}
+          onPress={send}
+        />
 
         <TouchableOpacity
           style={styles.backButton}
-          onPress={() => navigation.pop()}>
+          onPress={() => navigation.pop()}
+        >
           <Text style={styles.backButtonText}>Back</Text>
         </TouchableOpacity>
       </View>
-      <ScrollView style={styles.ScrollView}>
-        <FlatList
-          inverted={true}
-          style={{ width: "100%"}}
-          data={conversation}
-          keyExtractor={item => item.key}
-          renderItem={({ item }) => {
-            return (
-              <View
-                style={{
-                  elevation: 8,
-                  borderRadius: 15,
-                  backgroundColor: "#3E90D0",
-                  marginBottom: 15,
-                  padding: 20,
-                }}
-              >
-                <View>
-                  <Text>
-                    Message sent at: {item.timestamp}
-                  </Text>
-                  <Text>
-                    Message body: {item.body}{" "}
-                  </Text>
-                </View>
-              </View>
-            );
-          }}
-        />
-        <View>
-        </View>
-  
-    <Text style={styles.dummy}>dummy</Text>
-    </ScrollView>
-    </View>
+    </KeyboardAwareScrollView>
   );
-};
+}
 
 const styles = StyleSheet.create({
-  inputBackButtoncontainer: {
-    alignItems: "center"
-  },
-  input: {
-    justifyContent: 'center',
-    paddingRight: 40,
-    paddingLeft: 5,
-    paddingTop: 10,
-    marginTop: 30,
-    width: "80%", 
-    height:45,
-    borderRadius: 20,
-    backgroundColor: 'white',
-    borderWidth: 3,
-    borderColor: "lightgray"
-  },
-  inputWrapper:{
-    flex: 1,
-    alignItems: 'center',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    marginTop: 30,
-    marginBottom: 30
-  },
-  plusIcon: {
-    position: 'absolute',
-    marginTop: 30,
-    top: -27,
-    right: 14,
-    color: 'gray'
-   
+  // keyOpenContainer: {
+  //   marginBottom: "50%",
+  //   height: "50%",
+  // },
+  // input: {},
+  keyClosedContainer: {
+    marginTop: "90%",
+    backgroundColor: "lightgray",
   },
   backButton: {
     marginTop: 20,
-    marginBottom: 10,
+    marginBottom: 40,
     width: 60,
     height: 30,
     backgroundColor: "#092455",
     borderRadius: 30,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
-  backButtonText:{
-    color: 'white'
+  backButtonText: {
+    color: "white",
+    textAlign: "center",
+    // marginBottom: 40,
   },
-  dummy: {
-    marginTop: 200,
-    color: "lightgray"
-  }
 });
 
 function mapStateToProps(state) {
   const listOfMessages = _.map(state.messagesList.messagesList, (val, key) => {
     return {
       ...val,
-      key: key
+      key: key,
     };
   });
   return {
-    listOfMessages
+    listOfMessages,
   };
 }
 

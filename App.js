ea306ad9885f72e42b3f2abf0b4678626ae95dac
Frank Meza <frankmezapdx@gmail.com>
2020-05-11 22:30:21 -0700

    move most of the navigation into util functions

--- App.js
+++ App.js
@@ -1,49 +1,58 @@
-import React, { useState, useContext, useMemo } from "react";
-import { Button, Image, StyleSheet, Text, View, Screen } from "react-native";
+import React, { useState, useMemo } from "react";
+// import React, { useState, useContext, useMemo } from "react";
+import { Image, StyleSheet } from "react-native";
+// import { Button, Image, StyleSheet, Text, View, Screen } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { AuthContext } from "./context";
 import { SplashScreen } from "expo";
 import Footer from "./components/Footer";
-import ClientNavigation from "./navigation/ClientNavigation";
-import MainHeader from "./components/MainHeader";
-import NestedHeader from "./components/NestedHeader";
-import PilotNavigation from "./navigation/PilotNavigation";
-import SignUpNavigation from "./navigation/SignUpNavigation";
-import SignInScreen from "./screens/auth/SignInScreen";
-import LoadingScreen from "./screens/LoadingScreen";
+// import ClientNavigation from "./navigation/ClientNavigation";
+// import MainHeader from "./components/MainHeader";
+// import NestedHeader from "./components/NestedHeader";
+// import PilotNavigation from "./navigation/PilotNavigation";
+// import SignUpNavigation from "./navigation/SignUpNavigation";
+// import SignInScreen from "./screens/auth/SignInScreen";
+// import LoadingScreen from "./screens/LoadingScreen";
 import * as firebase from "firebase";
-import {
-  Ionicons,
-  Entypo,
-  FontAwesome5,
-  MaterialCommunityIcons,
-} from "@expo/vector-icons";
+// import {
+//   Ionicons,
+//   Entypo,
+//   FontAwesome5,
+//   MaterialCommunityIcons,
+// } from "@expo/vector-icons";
 // REDUX STUFF
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import ReduxThunk from "redux-thunk";
 import reducers from "./reducers/index";
+import {
+  clientNavigation,
+  pilotNavigation,
+  renderLogin,
+} from "./appNavigationUtils";
+// import { clientNavigationMetadata, pilotNavigationMetadata } from "./appNavigationUtils";
 
-const AuthStack = createStackNavigator();
-const RootClientStack = createStackNavigator();
-const RootPilotStack = createStackNavigator();
+// const AuthStack = createStackNavigator();
+// const RootClientStack = createStackNavigator();
+// const RootPilotStack = createStackNavigator();
 
 SplashScreen.preventAutoHide();
 setTimeout(SplashScreen.hide, 3500);
 
 export default () => {
+  console.disableYellowBox = true;
   // REDUX STATE
   const state = createStore(reducers, {}, applyMiddleware(ReduxThunk));
-  console.disableYellowBox = true;
+
+  let [ loggedIn, setLoggedIn ] = useState(false);
+  let [ userType, setUserType ] = useState(null);
+  // let [ headerType, setHeaderType ] = useState("main");
+
   // auth stuff - maybe should be elsewhere?
   const auth = firebase.auth();
 
-  let [loggedIn, setLoggedIn] = useState(false);
-  let [userType, setUserType] = useState(null);
-  let [headerType, setHeaderType] = useState('main');
-
-  auth.onAuthStateChanged((user) => {
+  auth.onAuthStateChanged(user => {
     if (user) {
       setLoggedIn(true);
       setUserType(user.photoURL);
@@ -53,19 +62,19 @@ export default () => {
     }
   });
 
-  function LogoTitle() {
-    return (
-      <Image
-        style={{
-          width: 290,
-          height: 55,
-          marginBottom: 10,
-          alignItems: "center",
-        }}
-        source={require("./assets/hovrtek_logo.png")}
-      />
-    );
-  }
+  // function LogoTitle() {
+  //   return (
+  //     <Image
+  //       style={{
+  //         width: 290,
+  //         height: 55,
+  //         marginBottom: 10,
+  //         alignItems: "center",
+  //       }}
+  //       source={require("./assets/hovrtek_logo.png")}
+  //     />
+  //   );
+  // }
 
   const authContext = useMemo(() => {
     return {
@@ -77,94 +86,106 @@ export default () => {
     };
   }, []);
 
+  const isClientLoggedIn = loggedIn && userType === "C";
+  const isPilotLoggedIn = loggedIn && userType === "P";
+
+
+  // const rootNavigationMeta = isClientLoggedIn
+  //   ? clientNavigationMetadata
+  //   : isPilotLoggedIn
+  //   ? pilotNavigationMetadata
+  //   :
+
   return (
     <Provider store={state}>
       <AuthContext.Provider value={authContext}>
         <NavigationContainer>
-          {loggedIn ? (
-            userType === "C" ? (
-              <RootClientStack.Navigator>
-                <RootClientStack.Screen
-                  name="Client"
-                  component={ClientNavigation}
-                  headerMode="screen"
-                  options={{
-                    title: "Home",
-                    headerTitle: () => <MainHeader />,
-                    headerStyle: {
-                      backgroundColor: "#092455"
-                    }
-                  }}
-                />
-              </RootClientStack.Navigator>
-            ) : userType === "P" ? (
-              <RootPilotStack.Navigator headerMode={"none"}>
-                <RootPilotStack.Screen
-                  name="Pilot"
-                  component={PilotNavigation}
-                  headerMode="screen"
-                />
-              </RootPilotStack.Navigator>
-            ) : (
-              <AuthStack.Navigator>
-                <AuthStack.Screen
-                  name="SignIn"
-                  component={SignInScreen}
-                  options={{
-                    title: "",
+          {isClientLoggedIn && clientNavigation}
+          {isPilotLoggedIn && pilotNavigation}
+          {!loggedIn && renderLogin()}
 
-                    headerStyle: {
-                      backgroundColor: "#092455",
-                      borderBottomWidth: 10,
-                      borderBottomColor: "grey",
-                      height: 110,
-                    },
-                  }}
-                />
-                <AuthStack.Screen
-                  name="SignUp"
-                  component={SignUpNavigation}
-                  options={{
-                    title: "",
-                    headerLeft: () => (
-                      <Button
-                        onPress={() => navigation.goBack()}
-                        style={styles.backButton}
-                        title="Back"
-                        color="#fff"
-                      />
-                    ),
-
-                    headerStyle: {
-                      backgroundColor: "#092455",
-                      borderBottomWidth: 10,
-                      borderBottomColor: "grey",
-                      height: 110,
-                    },
-                  }}
-                />
-                <AuthStack.Screen
-                  name="Loading"
-                  component={LoadingScreen}
-                  options={{
-                    title: "",
-                    headerStyle: {
-                      backgroundColor: "#092455",
-                      borderBottomWidth: 10,
-                      borderBottomColor: "grey",
-                      height: 110,
-                    },
-                  }}
-                />
-              </AuthStack.Navigator>
-            )
+          {/* {loggedIn ? userType === "C" ? (
+            <RootClientStack.Navigator>
+              <RootClientStack.Screen
+                name="Client"
+                component={ClientNavigation}
+                headerMode="screen"
+                options={{
+                  title: "Home",
+                  headerTitle: () => <MainHeader />,
+                  headerStyle: {
+                    backgroundColor: "#092455",
+                  },
+                }}
+              />
+            </RootClientStack.Navigator>
+          ) : userType === "P" ? (
+            <RootPilotStack.Navigator headerMode={"none"}>
+              <RootPilotStack.Screen
+                name="Pilot"
+                component={PilotNavigation}
+                headerMode="screen"
+              />
+            </RootPilotStack.Navigator>
           ) : (
             <AuthStack.Navigator>
               <AuthStack.Screen
                 name="SignIn"
                 component={SignInScreen}
                 options={{
-                  headerTitle: (props) => <LogoTitle {...props} />,
+                  title: "",
+
+                  headerStyle: {
+                    backgroundColor: "#092455",
+                    borderBottomWidth: 10,
+                    borderBottomColor: "grey",
+                    height: 110,
+                  },
+                }}
+              />
+              <AuthStack.Screen
+                name="SignUp"
+                component={SignUpNavigation}
+                options={{
+                  title: "",
+                  headerLeft: () => (
+                    <Button
+                      onPress={() => navigation.goBack()}
+                      style={styles.backButton}
+                      title="Back"
+                      color="#fff"
+                    />
+                  ),
+
+                  headerStyle: {
+                    backgroundColor: "#092455",
+                    borderBottomWidth: 10,
+                    borderBottomColor: "grey",
+                    height: 110,
+                  },
+                }}
+              />
+              <AuthStack.Screen
+                name="Loading"
+                component={LoadingScreen}
+                options={{
+                  title: "",
+                  headerStyle: {
+                    backgroundColor: "#092455",
+                    borderBottomWidth: 10,
+                    borderBottomColor: "grey",
+                    height: 110,
+                  },
+                }}
+              />
+            </AuthStack.Navigator>
+          ) : (
+            <AuthStack.Navigator>
+              <AuthStack.Screen
+                name="SignIn"
+                component={SignInScreen}
+                options={{
+                  headerTitle: props => <LogoTitle {...props} />,
                   headerStyle: {
                     backgroundColor: "#092455",
                     width: "100%",
@@ -210,7 +231,7 @@ export default () => {
                 })}
               />
             </AuthStack.Navigator>
-          )}
+          )} */}
           {/* <StaturBar barStyle="light-content" backgroundColor="#6a51ae" /> */}
           <Footer />
         </NavigationContainer>
@@ -219,21 +240,21 @@ export default () => {
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
 
-// MainHeader Logo
-const LogoTitle = () => {
-  return (
-    <Image
-      style={{ width: 130, height: 22, marginTop: 0 }}
-      source={require("./assets/hovrtek_logo.png")}
-    />
-  );
-};
+// // MainHeader Logo
+// const LogoTitle = () => {
+//   return (
+//     <Image
+//       style={{ width: 130, height: 22, marginTop: 0 }}
+//       source={require("./assets/hovrtek_logo.png")}
+//     />
+//   );
+// };
---
+++ appNavigationUtils.js
@@ -0,0 +1,120 @@
+import React from "react";
+import { Button, Image } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import ClientNavigation from "./navigation/ClientNavigation";
+import MainHeader from "./components/MainHeader";
+import PilotNavigation from "./navigation/PilotNavigation";
+import SignInScreen from "./screens/auth/SignInScreen";
+import SignUpNavigation from "./navigation/SignUpNavigation";
+import LoadingScreen from "./screens/LoadingScreen";
+
+const RootStack = createStackNavigator();
+
+const LogoTitle = () => {
+  const imageStyle = {
+    width: 290,
+    height: 55,
+    marginBottom: 10,
+    alignItems: "center",
+  };
+
+  return (
+    <Image style={imageStyle} source={require("./assets/hovrtek_logo.png")} />
+  );
+};
+
+const renderLogin = () => {
+  const signInScreen = (
+    <RootStack.Screen
+      name="SignIn"
+      component={SignInScreen}
+      options={{
+        headerTitle: props => <LogoTitle {...props} />,
+        headerStyle: {
+          backgroundColor: "#092455",
+          width: "100%",
+          borderBottomWidth: 10,
+          borderBottomColor: "grey",
+          height: 110,
+        },
+      }}
+    />
+  );
+
+  const signUpScreen = (
+    <RootStack.Screen
+      name="SignUp"
+      component={SignUpNavigation}
+      options={({ navigation }) => ({
+        title: "",
+        headerLeft: () => (
+          <Button
+            onPress={() => navigation.goBack()}
+            style={styles.backButton}
+            title="Back"
+            color="#fff"
+          />
+        ),
+        headerStyle: {
+          backgroundColor: "#092455",
+          borderBottomWidth: 10,
+          borderBottomColor: "grey",
+          height: 110,
+        },
+      })}
+    />
+  );
+
+  const loadingScreen = (
+    <RootStack.Screen
+      name="Loading"
+      component={LoadingScreen}
+      options={() => ({
+        title: "",
+        headerStyle: {
+          backgroundColor: "#092455",
+          borderBottomWidth: 10,
+          borderBottomColor: "grey",
+          height: 110,
+        },
+      })}
+    />
+  );
+
+  return (
+    <RootStack.Navigator>
+      {signInScreen}
+      {signUpScreen}
+      {loadingScreen}
+    </RootStack.Navigator>
+  );
+};
+
+const clientNavigation = (
+  <RootStack.Navigator>
+    <RootStack.Screen
+      name="Client"
+      component={ClientNavigation}
+      headerMode="screen"
+      options={{
+        title: "Home",
+        headerTitle: () => <MainHeader />,
+        headerStyle: {
+          backgroundColor: "#092455",
+        },
+      }}
+    />
+  </RootStack.Navigator>
+);
+
+const pilotNavigation = (
+  <RootStack.Navigator headerMode="none">
+    <RootStack.Screen
+      name="Pilot"
+      component={PilotNavigation}
+      headerMode="screen"
+    />
+  </RootStack.Navigator>
+);
+
+export { clientNavigation, pilotNavigation, renderLogin };

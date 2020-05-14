485263c4d666ab94211b7e584e8d4c4e3773d182
jay winebrenner <jaywinebrenner@jays-iMac-2.local>
2020-05-14 14:54:27 -0700

    prettify droneExpPicker and DroneTYpe

--- components/BioPicker.js
+++ components/BioPicker.js
@@ -1,82 +1,91 @@
-import React, { useState, useContext } from 'react';
-import { Text, View, Button, Modal, StyleSheet, Picker, TouchableOpacity, TextInput, Alert } from 'react-native';
-import { PassSetPersonalBio, PassPersonalBioState } from '../screens/pilot/PilotProfileSetupPageOneScreen';
+import React, { useState, useContext } from "react";
+import {
+  Text,
+  View,
+  Button,
+  Modal,
+  StyleSheet,
+  Picker,
+  TouchableOpacity,
+  TextInput,
+  Alert,
+} from "react-native";
+import {
+  PassSetPersonalBio,
+  PassPersonalBioState,
+} from "../screens/pilot/PilotProfileSetupPageOneScreen";
 import { useNavigation } from "@react-navigation/native";
-import { APP_STRINGS } from '../constants/index';
+import { APP_STRINGS } from "../constants/index";
 
 //REFACTORED with APP_STRINGS and TURNARY VIA FRANKS SPECIFICATIONS
 
 const BioPicker = () => {
+  const navigation = useNavigation();
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
-    const navigation = useNavigation();
-    const [isModalVisible, setIsModalVisible] = useState(false)
+  const setPersonalBio = useContext(PassSetPersonalBio);
+  const personalBio = useContext(PassPersonalBioState);
 
-    const setPersonalBio = useContext(PassSetPersonalBio)
-    const personalBio = useContext(PassPersonalBioState)
+  console.log("PERSONAL BIO", personalBio);
+  console.log("SET PERSONAL BIO", setPersonalBio);
 
-    console.log("PERSONAL BIO", personalBio)
-    console.log("SET PERSONAL BIO", setPersonalBio)
+  const openModal = () => {
+    setIsModalVisible(true);
+  };
 
-    const openModal = () => {
-        setIsModalVisible(true);
-    }
+  const closeModal = () => {
+    setIsModalVisible(false);
+  };
 
-    const closeModal = () => {
-        setIsModalVisible(false);
-    }
-
-    const renderPersonalBioButton = (buttonText = '') => {
-        const title = APP_STRINGS.openModal;
-        return (
-            <TouchableOpacity style={styles.button} onPress={openModal} title={title}>
-                <Text style={styles.buttonText}>{buttonText}</Text>
-            </TouchableOpacity>
-        )
-    }
-
-    const renderPersonalBio = (hasPersonalBio = false) => {
-        return hasPersonalBio
-            ? renderPersonalBioButton(APP_STRINGS.workExperienceSet)
-            : renderPersonalBioButton(APP_STRINGS.setWorkExperience);
-    };
-
-    const renderTextInput = (bio, setBio) => {
-
-        return (
-            <View>
-                <TextInput style={styles.input} onChangeText={setBio} value={bio} />
-            </View>
-        );
-    };
-
+  const renderPersonalBioButton = (buttonText = "") => {
+    const title = APP_STRINGS.openModal;
     return (
-        <View style={styles.container}>
-            
-            <Modal
-                transparent={true}
-                visible={isModalVisible}
-                animationType={'slide'}
-                onRequestClose={() => closeModal()}
-            >
-                <View style={styles.modalContainer}>
-                    <View style={styles.innerContainer}>
-                        <Text style={styles.modalText}>{APP_STRINGS.briefSummary}</Text>
-                    </View>
+      <TouchableOpacity style={styles.button} onPress={openModal} title={title}>
+        <Text style={styles.buttonText}>{buttonText}</Text>
+      </TouchableOpacity>
+    );
+  };
 
-                    {renderTextInput(personalBio, setPersonalBio)}
-                    
-                    <View styles={styles.cancelWrapper}>
-                        <Button
-                            onPress={() => closeModal()}
-                            title={APP_STRINGS.choose}
-                        >
-                        </Button>
-                    </View>
-                </View>
-            </Modal>
+  const renderPersonalBio = (hasPersonalBio = false) => {
+    return hasPersonalBio
+      ? renderPersonalBioButton(APP_STRINGS.workExperienceSet)
+      : renderPersonalBioButton(APP_STRINGS.setWorkExperience);
+  };
 
-            {renderPersonalBio(personalBio)}
-            {/* {
+  const renderTextInput = (bio, setBio) => {
+    return (
+      <View>
+        <TextInput style={styles.input} onChangeText={setBio} value={bio} />
+      </View>
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      <Modal
+        transparent={true}
+        visible={isModalVisible}
+        animationType={"slide"}
+        onRequestClose={() => closeModal()}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.innerContainer}>
+            <Text style={styles.modalText}>{APP_STRINGS.briefSummary}</Text>
+          </View>
+
+          {renderTextInput(personalBio, setPersonalBio)}
+
+          <View styles={styles.cancelWrapper}>
+            <Button
+              onPress={() => closeModal()}
+              title={APP_STRINGS.choose}
+            ></Button>
+          </View>
+        </View>
+      </Modal>
+
+      {renderPersonalBio(personalBio)}
+      {/* {
 
                 (personalBio)
                     ?
@@ -97,60 +106,55 @@ const BioPicker = () => {
                         </Text>
                     </TouchableOpacity>)
             } */}
-
-        </View>
-    );
-}
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-
-    modalContainer: {
-        height: 300,
-        justifyContent: 'center',
-        paddingTop: 10,
-        padding: 10,
-        backgroundColor: '#f5f5f5',
-        marginTop: 200,
-        marginLeft: 5,
-        marginRight: 5,
-        borderRadius: 5
-
-    },
-    innerContainer: {
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    picker: {
-        height: 50,
-        width: 150,
-
-    },
-    modalText: {
-        fontSize: 20
-    },
-    cancelWrapper: {
-    },
-    button: {
-        width: 250,
-        height: 50,
-        backgroundColor: "#092455",
-        borderRadius: 30,
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginTop: 10,
-        marginBottom: 30,
-        color: 'white'
-    },
-    buttonText: {
-        color: "white"
-    },
-    input: {
-        marginTop: 20,
-        height: 90,
-        borderColor: "gray",
-        borderWidth: 1,
-        marginBottom: 20,
-    }
+  modalContainer: {
+    height: 300,
+    justifyContent: "center",
+    paddingTop: 10,
+    padding: 10,
+    backgroundColor: "#f5f5f5",
+    marginTop: 200,
+    marginLeft: 5,
+    marginRight: 5,
+    borderRadius: 5,
+  },
+  innerContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  picker: {
+    height: 50,
+    width: 150,
+  },
+  modalText: {
+    fontSize: 20,
+  },
+  cancelWrapper: {},
+  button: {
+    width: 250,
+    height: 50,
+    backgroundColor: "#092455",
+    borderRadius: 30,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 10,
+    marginBottom: 30,
+    color: "white",
+  },
+  buttonText: {
+    color: "white",
+  },
+  input: {
+    marginTop: 20,
+    height: 90,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 20,
+  },
 });
 
-export default BioPicker
+export default BioPicker;
--- constants/index.js
+++ constants/index.js
@@ -1,20 +1,19 @@
-
 // DECONSTRUCTED SCREENS
 // Biopicker
 // AirMapPicker
 // PilotProfileSetupPageOneScreen
 
 const APP_STRINGS = {
-    briefSummary: "Please Give Us a Brief Summary of Your Work Experience",
-    choose: "Choose",
-    experienceFlying: 'Have you had experience flying over 400 Feet?',
-    insurance: 'Do You Have Valid Insurance?',
-    modelDrone: 'What Drone Model Do You Have?',
-    openModal: "Open modal",
-    saveAndContinue: 'Save and Continue',
-    setWorkExperience: "Please Set Work Experience",
-    workExperienceSet: "Work Experience Set",
-    yearsExperience: 'How Many Years Of Drone Experience Do You Have?',
+  briefSummary: "Please Give Us a Brief Summary of Your Work Experience",
+  choose: "Choose",
+  experienceFlying: "Have you had experience flying over 400 Feet?",
+  insurance: "Do You Have Valid Insurance?",
+  modelDrone: "What Drone Model Do You Have?",
+  openModal: "Open modal",
+  saveAndContinue: "Save and Continue",
+  setWorkExperience: "Please Set Work Experience",
+  workExperienceSet: "Work Experience Set",
+  yearsExperience: "How Many Years Of Drone Experience Do You Have?",
 };
 
 export { APP_STRINGS };

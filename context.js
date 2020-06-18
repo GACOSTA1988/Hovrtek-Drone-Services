d329aee39b4160027f0b82357db7c0aa45b6cdb4
Joseph Wangemann <fractalscape13@gmail.com>
2020-06-18 11:16:38 -0700

    clean up unused import statement

--- context.js
+++ context.js
@@ -1,4 +1,4 @@
-import React, { createContext } from "react";
+import { createContext } from "react";
 
 export const AuthContext = createContext();
 export const NotificationContext = createContext();

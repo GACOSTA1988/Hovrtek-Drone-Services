00e91a382db76d13ca1c1e8044131be2408ca231
Joseph Wangemann <fractalscape13@gmail.com>
2020-06-18 17:19:06 -0700

    add logic to conditonally render messaging tab icon with a dot if there are unread messages in both PilotTabs and ClientTabs

--- navigation/ClientTabs.js
+++ navigation/ClientTabs.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import ClientHomeStackNavigator from "./ClientHomeStackNavigator";
 import ClientProjectStackNavigator from "./ClientProjectStackNavigator";
 import MessagingNavigation from './MessagingNavigation';
-import ProjectScreensStackNavigator from './ProjectScreensStackNavigator';
-import ProjectListScreen from '../screens/client/ProjectListScreen';
 import { FontAwesome } from '@expo/vector-icons';
-import { Entypo } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
 import { FontAwesome5 } from '@expo/vector-icons';
+import * as firebase from "firebase";
+import { getMessages } from "../actions/messages";
+import { connect } from "react-redux";
+import _ from "lodash";
 
-function ClientTabs() {
+function ClientTabs(props) {
   const Tabs = createBottomTabNavigator();
+
+  useEffect(() => {
+    props.getMessages();
+  }, []);
+
+  let userID = null;
+  let unreadMessages = [];
+  const [ noteVisible, setNoteVisible ] = useState(false);
+  if (firebase.auth().currentUser && props.listOfMessages) {
+    userID = firebase.auth().currentUser.uid;
+    props.listOfMessages.forEach((message) => {
+      if (message.userTwoID === userID && !message.read) {
+        if (!unreadMessages.includes(message)) {
+          unreadMessages.push(message);
+        }
+      }
+    });
+  }
 
   return (
       <Tabs.Navigator tabBarOptions={{activeBackgroundColor: 'lightblue', inactiveBackgroundColor: '#092455', activeTintColor: '#092455', inactiveTintColor: 'white', showLabel: false}}>
@@ -31,11 +51,25 @@ function ClientTabs() {
         <Tabs.Screen 
           name="Messages" 
           component={MessagingNavigation}
-          options={{
-            tabBarIcon: ({color}) => (<Entypo name="message" size={24} color={color}/>)
-          }} />
+          options={unreadMessages.length > 0 ? {
+            tabBarIcon: ({color}) => (<Ionicons name="md-mail-unread" size={24} color={color} />) } : {
+              tabBarIcon: ({color}) => (<Ionicons name="md-mail" size={24} color={color} />) } }
+        />
       </Tabs.Navigator>
   );
 }
 
-export default ClientTabs;
+function mapStateToProps(state) {
+  const listOfMessages = _.map(state.messagesList.messagesList, (val, key) => {
+    return {
+      ...val,
+      key: key,
+    };
+  });
+  return {
+    listOfMessages,
+  };
+}
+
+export default connect(mapStateToProps, {
+  getMessages })(ClientTabs);

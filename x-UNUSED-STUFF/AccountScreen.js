54bb146a3baa79feab0bc61a26d89a039c92240f
Joseph Wangemann <fractalscape13@gmail.com>
2020-08-03 10:44:58 -0700

    comment out unused files

--- x-UNUSED-STUFF/AccountScreen.js
+++ x-UNUSED-STUFF/AccountScreen.js
@@ -1,23 +1,23 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+// import React from "react";
+// import { View, Text, StyleSheet } from "react-native";
 
-const AccountScreen = () => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.servicesText}>Account Screen</Text>
-    </View>
-  )
-};
+// const AccountScreen = () => {
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.servicesText}>Account Screen</Text>
+//     </View>
+//   )
+// };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  servicesText: {
-    fontSize:20
-  }
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: "center",
+//     alignItems: "center"
+//   },
+//   servicesText: {
+//     fontSize:20
+//   }
+// });
 
-export default AccountScreen;
+// export default AccountScreen;
--- x-UNUSED-STUFF/PilotDrawerContent.js
+++ x-UNUSED-STUFF/PilotDrawerContent.js
@@ -1,46 +1,46 @@
-import React from 'react';
-import { Text, View, SafeAreaView, TouchableOpacity, ScrollView } from 'react-native';
-import signOut from '../screens/auth/signOut';
+// import React from 'react';
+// import { Text, View, SafeAreaView, TouchableOpacity, ScrollView } from 'react-native';
+// import signOut from '../screens/auth/signOut';
 
 
-const PilotDrawerContent = () => {
-  return (
-    <SafeAreaView style={{flex: 1}}>
-      <ScrollView style={{marginLeft: 5}}>
-          <TouchableOpacity
-          style={{marginTop: 20}}
-          onPress={() => props.navigation.navigate('PilotTabs')}
-          >
-            <Text>Home</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-          style={{marginTop: 20}}
-          onPress={() => props.navigation.navigate('Profile')}
-          >
-            <Text>Profile</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-          style={{marginTop: 20}}
-          onPress={() => props.navigation.navigate('About')}
-          >
-            <Text>About</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-          style={{marginTop: 20}}
-          onPress={() => props.navigation.navigate('Support')}
-          >
-            <Text>Support</Text>
-          </TouchableOpacity>
-      </ScrollView>
+// const PilotDrawerContent = () => {
+//   return (
+//     <SafeAreaView style={{flex: 1}}>
+//       <ScrollView style={{marginLeft: 5}}>
+//           <TouchableOpacity
+//           style={{marginTop: 20}}
+//           onPress={() => props.navigation.navigate('PilotTabs')}
+//           >
+//             <Text>Home</Text>
+//           </TouchableOpacity>
+//           <TouchableOpacity
+//           style={{marginTop: 20}}
+//           onPress={() => props.navigation.navigate('Profile')}
+//           >
+//             <Text>Profile</Text>
+//           </TouchableOpacity>
+//           <TouchableOpacity
+//           style={{marginTop: 20}}
+//           onPress={() => props.navigation.navigate('About')}
+//           >
+//             <Text>About</Text>
+//           </TouchableOpacity>
+//           <TouchableOpacity
+//           style={{marginTop: 20}}
+//           onPress={() => props.navigation.navigate('Support')}
+//           >
+//             <Text>Support</Text>
+//           </TouchableOpacity>
+//       </ScrollView>
 
-      <TouchableOpacity
-      style={{marginTop: 20, marginLeft: 5}}
-      onPress={() => signOut()}
-      >
-        <Text>Sign out</Text>
-      </TouchableOpacity>
-    </SafeAreaView>
-  );
-}
+//       <TouchableOpacity
+//       style={{marginTop: 20, marginLeft: 5}}
+//       onPress={() => signOut()}
+//       >
+//         <Text>Sign out</Text>
+//       </TouchableOpacity>
+//     </SafeAreaView>
+//   );
+// }
 
-export default PilotDrawerContent;
+// export default PilotDrawerContent;
